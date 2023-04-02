baaffa656133a5d124cd41d371f5d02841daf935
Joas <joaoferreirapaula005@gmail.com>
2023-04-01 21:50:08 -0300

    To Do List/Gerenciador de tarefas
    Este programo foi o mais o complexo que já fiz até hoje, funciona como um gerenciador de tarefas comum com opções como adicionar item, remover item, editar item e ver lista, porém ele é capaz de armazenar as informações adicionadas em um arquivo txt separado, assim possibilitando ao usuário manter suas tarefas mesmo após fechar o programa. Sendo capaz de realizar todas as funções presentes no aplicativo com este backup salvo.
    Eu realizei este projeto no intuito de melhorar minhas capacidades com listas e arrays, aprender a mexer com arquivos separados dentro do python e também para praticar programar em si. 
    Fiquei muito satisfeito com o resultado e muito feliz de ter sido capaz de completa-lo. 😁

---
+++ toDo.py
@@ -0,0 +1,190 @@
+import os
+import time
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Funções --
+
+# Checando a existência da lista -
+def checkExistênciaBackup():
+    if os.path.isfile('tasks.txt'):
+        with open('tasks.txt', 'r') as file:
+            file.seek(0, os.SEEK_END)
+            isempty = file.tell() == 0
+            file.seek(0)
+        if isempty == False:
+            with open("tasks.txt", "r", encoding='utf=8') as backup:
+                for list in backup.readlines():
+                    data = list
+                    num, item = data.split(': ')
+                    toDo.append(item.capitalize())
+        else:
+            os.remove('tasks.txt')
+
+
+# Saindo do programa -
+def sair():
+    print(
+        '\033[31mVocê escolheu sair :(\033[0m\nObrigado por usar o nosso programa!\nSaindo...')
+    time.sleep(1.5)
+    exit()
+
+
+# Adcionando itens a lista -
+def add():
+    print('\033[32mVocê escolheu adcionar uma tarefa.\033[0m')
+    addItem = input('O que você gostaria de adicionar? ').capitalize()
+    itemAdicionado = addItem + '\n'
+    print()
+    if itemAdicionado in toDo:
+        print('\033[32mVocê já adcionou isto a sua lista!\033[0m')
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:  # Adição
+        toDo.append(addItem)
+        print(f'Item - {addItem}, adcionado a sua lista!')
+        print('---')
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        with open('tasks.txt', 'a', encoding='utf=8') as backup:
+            backup.write(str(len(toDo)) + ': ' + addItem + '\n')
+
+
+# Removendo itens do backup
+def removendoDoBackup(toDo, removendo):
+    remova = toDo.index(removendo) + 1
+    with open("tasks.txt", "r") as backup:
+        lines = backup.readlines()
+        pointer = 1
+        with open('tasks.txt', 'w') as backup:
+            for line in lines:
+                if pointer != remova:
+                    backup.write(line)
+                pointer += 1
+
+
+# Deletando a lista -
+def deletandoLista():
+    confirmacao = input(
+        'Tem certeza que deseja \033[31mapagar toda a lista\033[0m? ').lower()
+
+    if confirmacao == 'sim':  # Deletando toda lista e backup
+        print('Deletando toda sua lista...')
+        time.sleep(1)
+        toDo.clear()
+        open('tasks.txt', 'w')
+        print()
+        ver()
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    else:
+        print('\nUfa..\nEssa foi por pouco 😰')
+        time.sleep(3)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Removendo itens -
+def remover():
+    print('\033[31mVocê escolheu remover\033[0m')
+    print()
+    ver()
+    removerItem = input(
+        'O que você gostaria de remover (digite del para apagar toda lista)? ').capitalize()
+    removendo = removerItem + '\n'
+    print()    
+    if removendo in toDo or removerItem in toDo: # Garantindo que o item que quer ser reovido existe na lista e no backup
+        confirmacao = input(
+            f'Você tem certeza que deseja remver o item \033[31m{removerItem}\033[0m? ').lower()
+        if confirmacao == 'sim':
+            removendoDoBackup(toDo, removendo)
+            print(f'{removerItem} removido')
+            time.sleep(1)
+            toDo.remove(removendo)
+            ver()
+            time.sleep(3)
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:  # Não removendo item da lista
+            print('\033[32mNão removendo item.\033[0m')
+            time.sleep(1)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            remover()
+
+    elif removerItem == 'Del':
+        deletandoLista()
+
+    else:  # Caso o item que desejam remover não se encontre
+        print('Item não presente na lista.')
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# Imprimindo a lista -
+def ver():
+    print('Aqui está sua lista:\n')
+    for index in range(len(toDo)):
+        print(f'{index+1}: {toDo[index]}')
+    print("---\n")
+
+
+# Editando o backup - 
+def editandoBackup(editando, novo):
+    with open('tasks.txt', 'r') as backup:
+        conteudo = backup.read()
+    conteudo = conteudo.replace(editando, novo + '\n')
+    with open('tasks.txt', 'w') as backup:
+        backup.write(conteudo) 
+
+# Editando a lista -
+def editar():
+    ver()
+    editarItem = input(
+    'Informe o item que você gostaria de alterar: ') .capitalize()
+    editando = editarItem + '\n'
+
+    if editando in toDo:
+        novo = input(f'Você gostaria de mudar \033[34m{editarItem}\033[0m para o que? ') .capitalize()
+        for i in range(len(toDo)):
+            if toDo[i] == editando:
+                toDo[i] = novo  # Substituindo o valor de um item por um novo
+                print(f'\n{editarItem} alterado para {novo}.')
+                editandoBackup(editando, novo)
+    else:
+        print(f'\nItem: {editarItem} não encontrado\nTente denovo...')
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    time.sleep(3)
+    os.system('cls')
+
+# Código principal --
+toDo = []
+
+checkExistênciaBackup()
+
+print('\033[35mGerenciador de tarefas!\n---\n\033[0m')
+print('Gerenciador de tarefas simples para você sempre manter anotado o que você precisa fazer em seu dia!\n')
+
+
+while True:
+    print('O que você gostaria de fazer?')
+    menu = input(
+        'Adcionar (add), remover, editar, ver ou sair? ').lower()
+    print()
+    if menu.strip() == 'sair':
+        sair()
+    elif menu.strip() == 'add' or menu.strip() == 'adcionar':
+        add()
+    elif menu.strip() == 'remover':
+        remover()
+    elif menu.strip() == 'ver':
+        print('\033[35mVocê escolheu ver!\033[0m\n')
+        ver()
+        time.sleep(5)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    elif menu.strip() == 'editar':
+        print('\033[34mVocê escolheu editar.\n\033[0m')
+        editar()
+    else:
+        print('\033[31mOpção inválida!\033[0m\nTente novamente...')
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
